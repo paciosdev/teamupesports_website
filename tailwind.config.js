d2d6823f00e9e53cc275648e4ac7fc1020182392
Francesco Paciello <kekkopacio@gmail.com>
2024-05-13 23:51:09 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          customColor: '#6847FF', // Sostituisci con il tuo colore personalizzato
+        },
+      },
+    },
+    variants: {},
+    plugins: [],
+  };
+  
